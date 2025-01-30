3b32e53a0e78958095626bdae89d37467e70ac2d
Tshindimpho <mudzu13@gmail.com>
2025-01-30 14:34:34 +0200

    Implementing Pagination on the Table

--- src/app/students/students.component.ts
+++ src/app/students/students.component.ts
@@ -1,16 +1,16 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, ViewChild } from '@angular/core';
 import { StudentService } from '../Services/student.service';
 import { Student } from '../Models/Ui-models/student.model';
 import { MatTableDataSource } from '@angular/material/table'; 
 import { MatTableModule } from '@angular/material/table';  
 import { CommonModule } from '@angular/common';
-
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-students',
   standalone: true,
-  imports: [MatTableModule,CommonModule ],
-   templateUrl: './students.component.html',
+  imports: [MatTableModule, CommonModule, MatPaginatorModule],
+  templateUrl: './students.component.html',
   styleUrl: './students.component.css'
 })
 export class StudentsComponent implements OnInit {
@@ -18,10 +18,13 @@ export class StudentsComponent implements OnInit {
   students: Student[] = [];
   displayedColumns: string[] = ['firstName', 'lastName', 'dateofBith', 'email', 'phone', 'gender'];
 
+  // Create a prop for the paginator
+  @ViewChild(MatPaginator) matPaginator!: MatPaginator;
+
   // Define the data source
   dataSource: MatTableDataSource<Student> = new MatTableDataSource<Student>();
 
- // inject the service without needing a constructor
+  // inject the service without needing a constructor
   private studentService = inject(StudentService);
 
   ngOnInit(): void {
@@ -35,5 +38,11 @@ export class StudentsComponent implements OnInit {
         console.log(errorResponse);
       }
     );
+  }
+
+  ngAfterViewInit() {
+    if (this.matPaginator) {
+      this.dataSource.paginator = this.matPaginator;
+    }
   }
 }
