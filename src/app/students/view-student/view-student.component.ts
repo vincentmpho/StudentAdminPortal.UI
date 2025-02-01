9689637fbe22eaa7b4f2a86fadcf69ef943e8f6a
Tshindimpho <mudzu13@gmail.com>
2025-02-01 14:47:47 +0200

    Creating and Implementing Design for View Student

--- src/app/students/view-student/view-student.component.ts
+++ src/app/students/view-student/view-student.component.ts
@@ -1,36 +1,49 @@
 import { Component, OnInit } from '@angular/core';
 import { StudentService } from '../../Services/student.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @Component({
   selector: 'app-view-student',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIconModule, 
+    MatButtonModule, 
+    MatFormFieldModule, 
+    MatInputModule, 
+    MatDatepickerModule, 
+    MatNativeDateModule, 
+    RouterLink,
+    FormsModule
+  ],
   templateUrl: './view-student.component.html',
   styleUrl: './view-student.component.css'
 })
-export class ViewStudentComponent  implements OnInit{
+export class ViewStudentComponent implements OnInit {
+  studentId: string | null | undefined;
 
-//assign to  a local variable
-studentId : string | null | undefined;
-
-constructor(private readonly studentService: StudentService, private readonly route: ActivatedRoute) {}
+  constructor(
+    private readonly studentService: StudentService, 
+    private readonly route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-this.route.paramMap.subscribe(
-  (params)=>{
-   this.studentId =params.get('id');
+    this.route.paramMap.subscribe((params) => {
+      this.studentId = params.get('id');
 
-   //check
-   if(this.studentId){
-    this.studentService.getStudent(this.studentId)
-    .subscribe(
-     (successResponse)=>{
-        console.log(successResponse);
-     }
-    );
-   }
-  }
-);
+      if (this.studentId) {
+        this.studentService.getStudent(this.studentId).subscribe(
+          (successResponse) => {
+            console.log(successResponse);
+          }
+        );
+      }
+    });
   }
 }
