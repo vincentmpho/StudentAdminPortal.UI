aeff98a4e61ef58075a65c358398b2cb6346098c
Tshindimpho <mudzu13@gmail.com>
2025-02-02 23:29:59 +0200

    Binding Student Dat  from API to UI

--- src/app/students/view-student/view-student.component.ts
+++ src/app/students/view-student/view-student.component.ts
@@ -8,25 +8,46 @@ import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
+import { Student } from '../../Models/Ui-models/student.model';
 
 @Component({
   selector: 'app-view-student',
   standalone: true,
   imports: [
-    MatIconModule, 
-    MatButtonModule, 
-    MatFormFieldModule, 
-    MatInputModule, 
-    MatDatepickerModule, 
-    MatNativeDateModule, 
+    MatFormFieldModule,
+    MatInputModule,        // ← Provides matInput
+    MatDatepickerModule,   // ← Provides datepicker components
+    MatNativeDateModule,   // ← Required for datepicker
+    MatIconModule,
+    MatButtonModule,
     RouterLink,
-    FormsModule
+    FormsModule            // ← Required for ngModel
   ],
   templateUrl: './view-student.component.html',
   styleUrl: './view-student.component.css'
 })
 export class ViewStudentComponent implements OnInit {
   studentId: string | null | undefined;
+  student : Student = {
+    id: '',
+    firstName: '',
+    lastName: '',
+    dateOfBirth: '',
+    email: '',
+    phone: 0,
+    gengerId: '',
+    profileImageUrl: '',
+    gender: {
+      id: '',
+      description: ''
+    },
+    address: {
+      id: '',
+      physicalAddress: '',
+      postalAddress: ''
+    }
+
+  }
 
   constructor(
     private readonly studentService: StudentService, 
@@ -40,7 +61,7 @@ export class ViewStudentComponent implements OnInit {
       if (this.studentId) {
         this.studentService.getStudent(this.studentId).subscribe(
           (successResponse) => {
-            console.log(successResponse);
+            this.student=successResponse;
           }
         );
       }
